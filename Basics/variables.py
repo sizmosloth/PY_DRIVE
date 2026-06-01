0ad93430e8b0a7ce49f232fadefa44947ce23e99
sizmosloth <samaybagga8@gmail.com>
2026-06-01 21:22:31 +0530

    Made Variables.py file

---
+++ Basics/variables.py
@@ -0,0 +1,39 @@
+name = "Samay"
+age = 18
+weight = 65
+height = 5.8
+is_student = True
+nothing = None
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight)
+print("Height:", height)
+print("Is Student:", is_student)
+print("Nothing:", nothing)
+
+
+x, y, z = 1, 2, 3
+a = b = c = 100
+print("x:", x)
+print("y:", y)
+print("z:", z)
+print(a, b, c)
+
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(height))
+print(type(is_student))
+print(type(nothing))
+
+
+x = "100"
+x = int(x)
+print(type(x))
+y = 100
+y = str(y)
+print(type(y))
+z = "3.14"
+z = float(z)
+print(type(z))
