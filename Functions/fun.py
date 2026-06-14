c201c1811ef2492af3c321f4fa9e10ac6ff32c0e
sizmosloth <samaybagga8@gmail.com>
2026-06-14 11:10:44 +0530

    Learned about args, kwargs , local & global and function as a object in py

--- Functions/fun.py
+++ Functions/fun.py
@@ -41,3 +41,50 @@ def get_stats(numbers):
 
 low, high, total = get_stats([3, 6, 5, 7, 8, 9])
 print(low, high, total)
+
+#*ARGS MULTIPLE INPUTS LIKE IN SIDE A TUPLE FOR A FUNCTION---
+def makepizza(size, *toppings):
+    print(f"Make a {size} Pizza with toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+makepizza("medium", "cheese", "mushrooms", "corn")
+
+#**KWARGS LIKE MULTIPLE INPUTS BUT LIKE KEY AND VALUE PAIRS FOR A FUNCTION---
+##IT TAKES KEYWORD ARGUMENTS---
+def orderpizza(size, *toppings, **details):
+    print(f"Make a {size} Pizza with toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+    print(details)         #as a dict
+orderpizza("medium", "cheese", "mushrooms", "corn", payment = "COD", tip = "$5")
+
+def orderpizza(size, *toppings, **details):
+    print(f"Make a {size} Pizza with toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+    for key, value in details.items():
+        print(f"- {key} : {value}")
+orderpizza("medium", "cheese", "mushrooms", "corn", payment = "COD", tip = "$5")
+
+
+#USING FUNCTION TO CHANGE GLOBAL VARIABLE---
+var = 100 #Global var
+def change():
+    x = 50 # Local var
+    print(x)
+change()
+
+def change_global():
+    global x
+    x = 50
+change_global()
+print(x)
+
+#FUNCTIONS ARE TREATED AS OBJECTS IN PY---
+def sq(x):
+    return x ** 2
+
+def square(fun, num): # function as a argument
+    return fun(num)
+
+print(square(sq, 5))
