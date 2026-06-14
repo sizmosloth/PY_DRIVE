05320f0da9d638122b728396e079422d7318344f
sizmosloth <samaybagga8@gmail.com>
2026-06-14 10:55:50 +0530

    Learned about functions in python

---
+++ Functions/fun.py
@@ -0,0 +1,50 @@
+#FUNCTONS---
+
+def greet():
+    print("Hello, Greet Function")
+
+greet() #call
+
+def greet_user(name):
+    print(f"Hello, {name}")
+
+greet_user("Samay") #call
+
+def sum_num(a, b):
+    print(f"The sum of {a} and {b} is: {a + b}")
+
+sum_num(2, 3)
+
+def multiply(a, b):
+    return a * b
+
+result = multiply(4, 5)
+print(f"The product of 4 and 5 is: {result}")
+
+#DEFAULT PARAMETERS---
+def greet(name, message="Good Morning"):
+    print(f"{message}, {name}!")
+
+greet("Alice")  # Uses default message
+greet("Bob", "Hello")  # Overrides default message
+
+#RETURN VALUE---
+def get_fullname(first, last):
+    full_name = f"{first} {last}"
+    return full_name
+name = get_fullname("Samay", "Bagga")
+print(name)
+
+#RETURN MULTIPLE---
+def get_stats(numbers):
+    return min(numbers), max(numbers), sum(numbers)
+
+low, high, total = get_stats([3, 6, 5, 7, 8, 9])
+print(low, high, total)
+
+#ARGS MULTIPLE INPUTS LIKE IN SIDE A TUPLE FOR A FUNCTION---
+def makepizza(size, *toppings):
+    print(f"Make a {size} Pizza with toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+makepizza("medium", "cheese", "mushrooms", "corn")
